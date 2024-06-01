a7b73fcb4044d2ac0f67e488e101567c08d76cfa
fxfuren <nikitostimoshenko@gmail.com>
2024-06-01 14:07:12 +0300

    add inster db data and add notifaction with and

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Footer, Navbar } from "../components";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { supabase } from "../lib/supabase";
+import { notification } from 'antd';
+
 const Checkout = () => {
   const state = useSelector((state) => state.handleCart);
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    address: "",
+    address2: "",
+    city: "",
+    zip: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data, error } = await supabase.from("orders").insert(formData);
+      if (error) {
+        throw error;
+      }
+      console.log(data);
+      notification.success({
+        message: 'Заказ оформлен',
+        description: 'Ваш заказ успешно оформлен.',
+      });
+    } catch (error) {
+      console.error(error.message);
+      notification.error({
+        message: 'Ошибка',
+        description: 'Не удалось оформить ваш заказ. Пожалуйста, попробуйте еще раз.',
+      });
+    }
+  };
 
   const EmptyCart = () => {
     return (
@@ -19,6 +56,7 @@ const Checkout = () => {
       </div>
     );
   };
+
 
   const ShowCheckout = () => {
     let subtotal = 0;
@@ -43,18 +81,18 @@ const Checkout = () => {
                 <div className="card-body">
                   <ul className="list-group list-group-flush">
                     <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
-                      Товары ({totalItems})<span>${Math.round(subtotal)}</span>
+                      Товары ({totalItems})<span>{Math.round(subtotal)} тг.</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between align-items-center px-0">
                       Доставка
-                      <span>${shipping}</span>
+                      <span>{shipping} тг.</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
                       <div>
-                        <strong>Total amount</strong>
+                        <strong>Итого</strong>
                       </div>
                       <span>
-                        <strong>${Math.round(subtotal + shipping)}</strong>
+                        <strong>{Math.round(subtotal + shipping)} тг.</strong>
                       </span>
                     </li>
                   </ul>
@@ -67,17 +105,20 @@ const Checkout = () => {
                   <h4 className="mb-0">Расчетный адрес</h4>
                 </div>
                 <div className="card-body">
-                  <form className="needs-validation" novalidate>
+                  <form className="needs-validation" onSubmit={handleSubmit} noValidate>
                     <div className="row g-3">
                       <div className="col-sm-6 my-1">
-                        <label for="firstName" className="form-label">
+                        <label htmlFor="firstname" className="form-label">
                           Имя
                         </label>
                         <input
                           type="text"
                           className="form-control"
-                          id="firstName"
+                          id="firstname"
+                          name="firstname"
                           placeholder=""
+                          value={formData.firstname}
+                          onChange={handleChange}
                           required
                         />
                         <div className="invalid-feedback">
@@ -86,14 +127,17 @@ const Checkout = () => {
                       </div>
 
                       <div className="col-sm-6 my-1">
-                        <label for="lastName" className="form-label">
+                        <label htmlFor="lastname" className="form-label">
                           Фамилия
                         </label>
                         <input
                           type="text"
                           className="form-control"
-                          id="lastName"
+                          id="lastname"
+                          name="lastname"
                           placeholder=""
+                          value={formData.lastname}
+                          onChange={handleChange}
                           required
                         />
                         <div className="invalid-feedback">
@@ -102,14 +146,17 @@ const Checkout = () => {
                       </div>
 
                       <div className="col-12 my-1">
-                        <label for="email" className="form-label">
+                        <label htmlFor="email" className="form-label">
                           Почта
                         </label>
                         <input
                           type="email"
                           className="form-control"
                           id="email"
+                          name="email"
                           placeholder="you@example.com"
+                          value={formData.email}
+                          onChange={handleChange}
                           required
                         />
                         <div className="invalid-feedback">
@@ -118,14 +165,17 @@ const Checkout = () => {
                       </div>
 
                       <div className="col-12 my-1">
-                        <label for="address" className="form-label">
+                        <label htmlFor="address" className="form-label">
                           Адресс
                         </label>
                         <input
                           type="text"
                           className="form-control"
                           id="address"
+                          name="address"
                           placeholder="1234 Main St"
+                          value={formData.address}
+                          onChange={handleChange}
                           required
                         />
                         <div className="invalid-feedback">
@@ -134,76 +184,83 @@ const Checkout = () => {
                       </div>
 
                       <div className="col-12">
-                        <label for="address2" className="form-label">
+                        <label htmlFor="address2" className="form-label">
                           Адресс 2{" "}
-                          <span className="text-muted">(Optional)</span>
+                          <span className="text-muted">(Опцианально)</span>
                         </label>
                         <input
                           type="text"
                           className="form-control"
                           id="address2"
+                          name="address2"
                           placeholder="Apartment or suite"
-                        />
-                      </div>
-                      <div className="col-md-4 my-1">
-                        <label for="state" className="form-label">
-                          Город
-                        </label>
-                        <br />
-                        <select className="form-select" id="state" required>
-                          <option value="">Выберите...</option>
-                          <option>Уральск</option>
-                        </select>
-                        <div className="invalid-feedback">
-                        Пожалуйста, укажите действительный город.
+                          value={formData.address2}
+                          onChange={handleChange}
+                          />
+                          </div>
+                          <div className="col-md-4 my-1">
+                            <label htmlFor="city" className="form-label">
+                              Город
+                            </label>
+                            <br />
+                            <select
+                              className="form-select"
+                              id="city"
+                              name="city"
+                              value={formData.city}
+                              onChange={handleChange}
+                              required
+                            >
+                              <option value="">Выберите...</option>
+                              <option>Уральск</option>
+                            </select>
+                            <div className="invalid-feedback">Пожалуйста, укажите действительный город.</div>
+                          </div>
+    
+                          <div className="col-md-3 my-1">
+                            <label htmlFor="zip" className="form-label">
+                              Почтовый индекс
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="zip"
+                              name="zip"
+                              placeholder=""
+                              value={formData.zip}
+                              onChange={handleChange}
+                              required
+                            />
+                            <div className="invalid-feedback">Почтовый индекс обязателен.</div>
+                          </div>
                         </div>
-                      </div>
-
-                      <div className="col-md-3 my-1">
-                        <label for="zip" className="form-label">
-                          Почтовый индекс
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="zip"
-                          placeholder=""
-                          required
-                        />
-                        <div className="invalid-feedback">
-                          Почтовый индекс обязателен.
-                        </div>
-                      </div>
+    
+                        <hr className="my-4" />
+    
+                        <button className="w-100 btn btn-primary " type="submit">
+                          Продолжить заказ
+                        </button>
+                      </form>
                     </div>
-
-                    <hr className="my-4" />
-
-                    <button
-                      className="w-100 btn btn-primary "
-                      type="submit" disabled
-                    >
-                      Continue to checkout
-                    </button>
-                  </form>
+                  </div>
                 </div>
               </div>
             </div>
+          </>
+        );
+      };
+    
+      return (
+        <>
+          <Navbar />
+          <div className="container my-3 py-3">
+            <h1 className="text-center">Оформление заказа</h1>
+            <hr />
+            {state.length ? <ShowCheckout /> : <EmptyCart />}
           </div>
-        </div>
-      </>
-    );
-  };
-  return (
-    <>
-      <Navbar />
-      <div className="container my-3 py-3">
-        <h1 className="text-center">Checkout</h1>
-        <hr />
-        {state.length ? <ShowCheckout /> : <EmptyCart />}
-      </div>
-      <Footer />
-    </>
-  );
-};
-
-export default Checkout;
+          <Footer />
+        </>
+      );
+    };
+    
+    export default Checkout;
